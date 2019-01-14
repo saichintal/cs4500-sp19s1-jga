f19ac3b4c92a193f3db144af77321317054f81a7
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2019-01-13 20:22:21 -0500

    first commit

---
+++ src/main/java/com/example/cs4500sp19s1jga/controllers/hello/HelloObject.java
@@ -0,0 +1,17 @@
+package com.example.cs4500sp19s1jga.controllers.hello;
+
+public class HelloObject {
+	private String message;
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public HelloObject(String message) {
+		this.message = message;
+	}
+	public HelloObject() {
+	}
+}
+
