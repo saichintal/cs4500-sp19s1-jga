de738b1caf328e1174da08cdbed5aac018a4fdc7
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2019-01-23 17:37:01 -0500

    Added the User model

---
+++ src/main/java/com/example/cs4500sp19/models/User.java
@@ -0,0 +1,18 @@
+package com.example.cs4500sp19.models;
+
+public class User {
+	private Integer id;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	// provide a public set and get method for each of the class variables above
+	// make sure to use proper naming conventions for Java methods, e.g.,
+	// methods should be camelcase. Here's an example using the firstName variable:
+ 	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+}
