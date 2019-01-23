a4d7bff2d37f9bada4833e7f61adfb4dce0c8809
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2019-01-23 17:47:00 -0500

    Added endpoint to find user by id

--- src/main/java/com/example/cs4500sp19/services/UserService.java
+++ src/main/java/com/example/cs4500sp19/services/UserService.java
@@ -21,4 +21,14 @@ public class UserService {
 	public List<User> findAllUsers() {
 		return users;
 	}
+
+	@GetMapping("/api/user/{userId}")
+	public User findUserById(@PathVariable("userId") Integer id) {
+		for (User user : users) {
+			if (user.getId() == id) {
+				return user;
+			}
+		}
+		return null;
+	}
 }
